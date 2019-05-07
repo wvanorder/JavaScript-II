c5d68a595f9b8ddd7011fd26a01c1f586a7fbfee
VanOrder <vanorder@VanOrders-iMac.local>
2019-05-07 13:15:02 -0800

    finished all stretch goals except increment and decrement

--- assignments/closure.js
+++ assignments/closure.js
@@ -25,16 +25,42 @@ Closure();
 
 
 // ==== Challenge 2: Create a counter function ====
+var theCount = 0;
+
 const counter = () => {
+  function add() {
+   return theCount += 1;
+  }
   // Return a function that when invoked increments and returns a counter variable.
+  return add();
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
+console.log(counter());
+console.log(counter());
+counter();
+counter();
+console.log(counter());
+
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+var theNewCount = 0;
+
+const counterFactory = (variable) => {
+  const calculator = {
+    increment() {
+      return theNewCount += 1;
+    },
+    decrement() {
+      return theNewCount -= 1;
+    }
+  }
+  let upOrDown = `calculator.${variable}()`;
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+ return upOrDown();
 };
+
+console.log(counterFactory(increment));
